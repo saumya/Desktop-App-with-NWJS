be35cc1ad0235b8d853917628d4d5b29658f4806
saumya <2saumyaray@gmail.com>
2017-06-16 22:31:08 +0530

    wip: UI window is now almost fullscreen everywhere

--- main.js
+++ main.js
@@ -4,9 +4,14 @@
 //var Realm = require('realm');
 //console.log(Realm);
 
+nw.Screen.Init();
+var screenRect = nw.Screen.screens[0].work_area;
+var screenMargin = 100;//actual margin would be 100/2 on both sides
+
 var windowOptions = { 
-	width:400, 
-	height:600
+	width:(screenRect.width-screenMargin), 
+	height:(screenRect.height-screenMargin),
+	x:80,y:60
 };
 // UI Application
 //nw.Window.open('index.html', windowOptions , function(win) {});
@@ -15,4 +20,6 @@ var windowOptions = {
 //nw.Window.open('home.html', windowOptions , function(win) {});
 
 // React App
-nw.Window.open('home-react.html', windowOptions , function(win) {  });
+nw.Window.open('home-react.html', windowOptions , function(win) { 
+																										//win.moveTo(50,40); 
+																									});
