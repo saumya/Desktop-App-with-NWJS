9de41b70a923ee0b34120e4d5e80132097d39e4b
saumya <2saumyaray@gmail.com>
2017-06-21 12:29:29 +0530

    wip: Login Component, styling

--- react/src/component/Login.component.js
+++ react/src/component/Login.component.js
@@ -2,8 +2,19 @@
 import React, {Component} from 'react'
 
 function Login(props){
+    var letterStyle = {
+        padding: 10,
+        margin: 10,
+        backgroundColor: "#ffde00",
+        color: "#333",
+        display: "inline-block",
+        fontFamily: "monospace",
+        fontSize: "32",
+        textAlign: "center"
+      };
+
   return(
-    <div>
+    <div style={letterStyle}>
       <h1>Login</h1>
         <ul>
             <li>User Name: <input type="text" name="fname" /></li>
